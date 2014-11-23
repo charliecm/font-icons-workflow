883013f113768d1286f31aed6d26c41a496ab625
Charlie Chao <charliechao@outlook.com>
2014-11-23 13:17:13 -0800

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		shell: {
+			exportIcons: {
+				command: 'sketchtool export slices assets/icons.sketch --output=icons/'
+			}
+		},
+		webfont: {
+			icons: {
+				src: 'assets/icons/*.svg',
+				dest: 'src/fonts/',
+				destCss: 'src/css/',
+				options: {
+					font: 'icons',
+					fontHeight: 96,
+					normalize: false,
+					ascent: 84,
+					descent: 12,
+					destHtml: 'assets/',
+					templateOptions: {
+						classPrefix: 'icon-'
+					}
+				}
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-webfont');
+	grunt.loadNpmTasks('grunt-shell');
+
+	grunt.registerTask('default', [ 'shell:exportIcons', 'webfont:icons' ]);
+
+};
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Font Icons Workflow Sample Project
+
+The sample project from the article [Font Icons Workflow with Sketch and Grunt]().
+
+Run `npm install` to install the required dependencies.
+
+Run `grunt` to compile the icon fonts and generate the CSS file.
