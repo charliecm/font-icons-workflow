3c7078b5446e908bbbe0be0364f44f3b84af747f
Charlie Chao <charliechao@outlook.com>
2014-11-23 19:21:10 -0800

    Clean ups.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,34 +1,34 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-		shell: {
-			exportIcons: {
-				command: 'sketchtool export slices assets/icons.sketch --output=assets/icons/'
-			}
-		},
-		webfont: {
-			icons: {
-				src: 'assets/icons/*.svg',
-				dest: 'src/fonts/',
-				destCss: 'src/css/',
-				options: {
-					font: 'icons',
-					fontHeight: 96,
-					normalize: false,
-					ascent: 84,
-					descent: 12,
-					destHtml: 'assets/',
-					templateOptions: {
-						classPrefix: 'icon-'
-					}
-				}
-			}
-		}
-	});
+    grunt.initConfig({
+        shell: {
+            exportIcons: {
+                command: 'sketchtool export slices assets/icons.sketch --output=assets/icons/'
+            }
+        },
+        webfont: {
+            icons: {
+                src: 'assets/icons/*.svg',
+                dest: 'src/fonts/',
+                destCss: 'src/css/',
+                options: {
+                    font: 'icons',
+                    fontHeight: 96,
+                    normalize: false,
+                    ascent: 84,
+                    descent: 12,
+                    destHtml: 'assets/',
+                    templateOptions: {
+                        classPrefix: 'icon-'
+                    }
+                }
+            }
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-webfont');
-	grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-webfont');
+    grunt.loadNpmTasks('grunt-shell');
 
-	grunt.registerTask('default', [ 'shell:exportIcons', 'webfont:icons' ]);
+    grunt.registerTask('default', [ 'shell:exportIcons', 'webfont:icons' ]);
 
 };
